ac048d1e9512e12b6c515c49a6a68b5290d0c9fc
Moustafa El-Sayed <moustafa.mkms@gmail.com>
2019-12-28 22:02:14 +0200

    added payment

--- validation.js
+++ validation.js
@@ -41,3 +41,39 @@ const validatePassword = (password) => {
            /[0-9]/       .test(password) &&
            password.length > 8;
 }
+const validatePayment = (number , holderName , amount, expdate)=> {
+	alert ("henaa");
+	/*var today = new Date();
+	var dd = String(today.getDate()).padStart(2, '0');
+	var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+	var yyyy = today.getFullYear();
+	var d1 = new Date();
+	expdate = new Date();
+	var same = d1.getFullYear===expdate;*/
+ //today = mm + '/' + dd + '/' + yyyy;
+ var expdate = new Date(); 
+//# => Fri May 20 2016 16:09:43 GMT+0530 (India Standard Time)
+
+
+var date2 = new Date();
+date2.setDate(date.getDate() - 1); 
+//# => Thu May 19 2016 16:09:43 GMT+0530 (India Standard Time)
+
+	if(number.length!=15) {
+		alert("Card number invalid!");
+		return false;
+	}
+	else if (holderName.length <1) {
+		alert ("name is empty!");
+		return false;
+	}
+	else if (amount<1) {
+		alert("invalid data!");
+		return false;
+	}
+	else if (date2 < expdate ) {
+		alert("ye7ra2 talata kosomeeenek");
+		return false;
+
+	}
+}
